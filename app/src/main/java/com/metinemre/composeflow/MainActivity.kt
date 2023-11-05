37110ed37fb2c58ccdecf46c2062adc5b7862c34
metinemre <54211029+metinemredonmez@users.noreply.github.com>
2023-11-05 22:28:32 +0300

    updt finish kotlin flow
    updated

--- app/src/main/java/com/metinemre/composeflow/MainActivity.kt
+++ app/src/main/java/com/metinemre/composeflow/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.metinemre.composeflow.ui.theme.ComposeFlowTheme
 import com.metinemre.composeflow.viewmodel.MyViewModel
@@ -26,19 +32,82 @@ class MainActivity : ComponentActivity() {
             ComposeFlowTheme {
                 //comp
                 val viewModel : MyViewModel by viewModels()
-                val counter  = viewModel.countDownTimerFlow.collectAsState(initial = 10)
-
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Box(modifier = Modifier.fillMaxSize()){
-                        Text(text = counter.value.toString(),
-                            fontSize = 26.sp,
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.align(Alignment.Center)
-                            )
-                    }
-                }
+                SecondScreen(viewModel = viewModel)
             }
+        }
+    }
+}
+
+
+@Composable
+fun FirstScreen(viewModel: MyViewModel) {
+    val counter  = viewModel.countDownTimerFlow.collectAsState(initial = 10)
+
+    // A surface container using the 'background' color from the theme
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Box(modifier = Modifier.fillMaxSize()){
+            Text(text = counter.value.toString(),
+                fontSize = 26.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(viewModel: MyViewModel) {
+    val liveDataValue = viewModel.livedata.observeAsState()
+    val stateFlowValue = viewModel.stateFlow.collectAsState()
+    val sharedFlowValue  = viewModel.sharedFlow.collectAsState(initial = "")
+
+
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Box(modifier = Modifier.fillMaxSize()){
+           Column(modifier = Modifier.align(Alignment.Center)){
+//
+               Text(text = liveDataValue.value ?: "",
+                   fontSize = 26.sp
+               )
+
+               Button(onClick = {
+                   viewModel.changeLiveDataValue()
+               }) {
+                   Text(text = "live data button clicked",
+                       fontSize = 20.sp
+                       )
+               }
+               Spacer(modifier = Modifier.padding(10.dp))
+
+
+               Text(text = stateFlowValue.value ?: "",
+                   fontSize = 26.sp
+               )
+
+               Button(onClick = {
+                   viewModel.changeStateFlowValue()
+               }) {
+                   Text(text = "state flow button clicked",
+                       fontSize = 20.sp
+                   )
+               }
+
+               Spacer(modifier = Modifier.padding(10.dp))
+
+
+               Text(text = sharedFlowValue.value ?: "",
+                   fontSize = 26.sp
+               )
+
+               Button(onClick = {
+                   viewModel.changedSahredFlowValue()
+               }) {
+                   Text(text = "shared data button clicked",
+                       fontSize = 20.sp
+                   )
+               }
+
+           }
         }
     }
 }
--- app/src/main/java/com/metinemre/composeflow/viewmodel/MyViewModel.kt
+++ app/src/main/java/com/metinemre/composeflow/viewmodel/MyViewModel.kt
@@ -1,8 +1,14 @@
 package com.metinemre.composeflow.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
@@ -52,6 +58,35 @@ class MyViewModel:ViewModel() {
 //                delay(50000)
 //                println("counter : ${it}")
 //            }
+        }
+
+    }
+
+    // different flows
+    // live data comparisons
+    private val _livedata = MutableLiveData<String>("kotlinlivedata")
+    val livedata : LiveData<String> =  _livedata
+
+
+    fun changeLiveDataValue() {
+        _livedata.value = "Live Data"
+    }
+
+    // flows
+    private val _stateFlow = MutableStateFlow("Kotlinstateflow")
+    val stateFlow = _stateFlow.asStateFlow()
+
+    private val _sharedFlow = MutableSharedFlow<String>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+
+    fun changeStateFlowValue(){
+        _stateFlow.value = "state flow"
+    }
+
+    fun changedSahredFlowValue() {
+        viewModelScope.launch {
+            _sharedFlow.emit("shared flow")
         }
 
     }
